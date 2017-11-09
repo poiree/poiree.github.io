ece74af016379a20105c9863bd513ad7ceb8a17e
poiree <chkeka@ukr.net>
2017-11-09 20:37:51 +0200

    corrected

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 	var i = 0,
 		j = 0;
 	while (i < 4) {
-		$('table').append("<tr></tr>");
-		while (j < 7) {
+		$('table').append($('tr'));
+		/*while (j < 7) {
 			$('tr:nth-of-type(' + (i + 1) + ')').append("<td></td>");
 			j++;
-		}
+		}*/
 		i++;
 	}
 });
