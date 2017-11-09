00ba1674dd0dd3587cd0b39bc4c6c2575b0694c5
poiree <chkeka@ukr.net>
2017-11-09 20:24:01 +0200

    corrected table-cells forming

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 	var i = 0,
 		j = 0;
 	while (i < 4) {
-		$('table').html(<tr></tr>);
+		$('table').append(<tr></tr>);
 		while (j < 7) {
-			$('tr:nth-of-type(' + (i + 1) + ')').html(<td></td>);
+			$('tr:nth-of-type(' + (i + 1) + ')').append(<td></td>);
 			j++;
 		}
 		i++;
