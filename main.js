6b19da1f2ddb76287600cceed135f12d0a673ee3
poiree <chkeka@ukr.net>
2017-11-09 20:21:41 +0200

    table-cell forming

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-	
+	var i = 0,
+		j = 0;
+	while (i < 4) {
+		$('table').html(<tr></tr>);
+		while (j < 7) {
+			$('tr:nth-of-type(' + (i + 1) + ')').html(<td></td>);
+			j++;
+		}
+		i++;
+	}
 });
