1e3bad5fd2e905bb4cd2cda3ac61b4134fd43cd0
poiree <chkeka@ukr.net>
2018-03-11 18:53:15 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,133 @@
-jQuery(document).ready(function($){var gallery=$('.cd-gallery'),foldingPanel=$('.cd-folding-panel'),mainContent=$('.cd-main');gallery.on('click','a',function(event){event.preventDefault();openItemInfo($(this).attr('href'));});foldingPanel.on('click','.cd-close',function(event){event.preventDefault();toggleContent('',false);});gallery.on('click',function(event){if($(event.target).is('.cd-gallery')&&$('.fold-is-open').length>0)toggleContent('',false);}) function openItemInfo(url){var mq=viewportSize();if(gallery.offset().top>$(window).scrollTop()&&mq!='mobile'){$('body,html').animate({'scrollTop':gallery.offset().top},100,function(){toggleContent(url,true);});}else if(gallery.offset().top+gallery.height()<$(window).scrollTop()+$(window).height()&&mq!='mobile'){$('body,html').animate({'scrollTop':gallery.offset().top+gallery.height()-$(window).height()},100,function(){toggleContent(url,true);});}else{toggleContent(url,true);}} function toggleContent(url,bool){if(bool){var foldingContent=foldingPanel.find('.cd-fold-content');foldingContent.load(url+' .cd-fold-content > *',function(event){setTimeout(function(){$('body').addClass('overflow-hidden');foldingPanel.addClass('is-open');mainContent.addClass('fold-is-open');},100);});}else{var mq=viewportSize();foldingPanel.removeClass('is-open');mainContent.removeClass('fold-is-open');(mq=='mobile'||$('.no-csstransitions').length>0)?$('body').removeClass('overflow-hidden'):mainContent.find('.cd-item').eq(0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){$('body').removeClass('overflow-hidden');mainContent.find('.cd-item').eq(0).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');});}} function viewportSize(){return window.getComputedStyle(document.querySelector('.cd-main'),'::before').getPropertyValue('content').replace(/"/g,"").replace(/'/g,"");} $('.cd-main-nav').on('click',function(event){if($(event.target).is('.cd-main-nav'))$(this).children('ul').toggleClass('is-visible');});var form=$('#ajax-contact');var formMessages=$('#form-messages');$('input, textarea').on('focus',function(){if($(formMessages).hasClass('success')){$(formMessages).removeClass('success');$(formMessages).text('');}});$(form).submit(function(event){event.preventDefault();var formData=$(form).serialize();$.ajax({type:'POST',url:$(form).attr('action'),data:formData}).done(function(response){$(formMessages).removeClass('error');$(formMessages).addClass('success');$(formMessages).text(response);$('#name').val('');$('#email').val('');$('#message').val('');}).fail(function(data){$(formMessages).removeClass('success');$(formMessages).addClass('error');if(data.responseText!==''){$(formMessages).text(data.responseText);}else{$(formMessages).text('Oops! An error occured and your message could not be sent.');}});});});
+jQuery(document).ready(function($){
+	var gallery = $('.cd-gallery'),
+		foldingPanel = $('.cd-folding-panel'),
+		mainContent = $('.cd-main');
+	/* open folding content */
+	gallery.on('click', 'a', function(event){
+		event.preventDefault();
+		openItemInfo($(this).attr('href'));
+	});
+
+	/* close folding content */
+	foldingPanel.on('click', '.cd-close', function(event){
+		event.preventDefault();
+		toggleContent('', false);
+	});
+	gallery.on('click', function(event){
+		/* detect click on .cd-gallery::before when the .cd-folding-panel is open */
+		if($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0 ) toggleContent('', false);
+	})
+
+	function openItemInfo(url) {
+		var mq = viewportSize();
+		if( gallery.offset().top > $(window).scrollTop() && mq != 'mobile') {
+			/* if content is visible above the .cd-gallery - scroll before opening the folding panel */
+			$('body,html').animate({
+				'scrollTop': gallery.offset().top
+			}, 100, function(){
+	           	toggleContent(url, true);
+	        });
+	    } else if( gallery.offset().top + gallery.height() < $(window).scrollTop() + $(window).height()  && mq != 'mobile' ) {
+			/* if content is visible below the .cd-gallery - scroll before opening the folding panel */
+			$('body,html').animate({
+				'scrollTop': gallery.offset().top + gallery.height() - $(window).height()
+			}, 100, function(){
+	           	toggleContent(url, true);
+	        });
+		} else {
+			toggleContent(url, true);
+		}
+	}
+
+	function toggleContent(url, bool) {
+		if( bool ) {
+			/* load and show new content */
+			var foldingContent = foldingPanel.find('.cd-fold-content');
+			foldingContent.load(url+' .cd-fold-content > *', function(event){
+				setTimeout(function(){
+					$('body').addClass('overflow-hidden');
+					foldingPanel.addClass('is-open');
+					mainContent.addClass('fold-is-open');
+				}, 100);
+
+			});
+		} else {
+			/* close the folding panel */
+			var mq = viewportSize();
+			foldingPanel.removeClass('is-open');
+			mainContent.removeClass('fold-is-open');
+
+			(mq == 'mobile' || $('.no-csstransitions').length > 0 )
+				/* according to the mq, immediately remove the .overflow-hidden or wait for the end of the animation */
+				? $('body').removeClass('overflow-hidden')
+
+				: mainContent.find('.cd-item').eq(0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					$('body').removeClass('overflow-hidden');
+					mainContent.find('.cd-item').eq(0).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+				});
+		}
+
+	}
+
+	function viewportSize() {
+		/* retrieve the content value of .cd-main::before to check the actua mq */
+		return window.getComputedStyle(document.querySelector('.cd-main'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
+	}
+
+
+	//open-close submenu on mobile
+	$('.cd-main-nav').on('click', function(event){
+		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
+	});
+
+	// Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    $('input, textarea').on('focus', function() {
+    	if ($(formMessages).hasClass('success')) {
+			$(formMessages).removeClass('success');
+	    	$(formMessages).text('');
+    	}
+    });
+    // Set up an event listener for the contact form.
+	$(form).submit(function(event) {
+	    // Stop the browser from submitting the form.
+	    event.preventDefault();
+
+	    // Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+		    type: 'POST',
+		    url: $(form).attr('action'),
+		    data: formData
+		}).done(function(response) {
+		    // Make sure that the formMessages div has the 'success' class.
+		    $(formMessages).removeClass('error');
+		    $(formMessages).addClass('success');
+
+		    // Set the message text.
+		    $(formMessages).text(response);
+
+		    // Clear the form.
+		    $('#name').val('');
+		    $('#email').val('');
+		    $('#message').val('');
+		}).fail(function(data) {
+		    // Make sure that the formMessages div has the 'error' class.
+		    $(formMessages).removeClass('success');
+		    $(formMessages).addClass('error');
+
+		    // Set the message text.
+		    if (data.responseText !== '') {
+		        $(formMessages).text(data.responseText);
+		    } else {
+		        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+		    }
+		});
+	});
+});
