7aec39e5f3796fcf30ac15af57a081ec5fea1a70
poiree <chkeka@ukr.net>
2018-05-29 00:20:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-    $('.dropdown-menu.show>.dropdown-toggle').on('click', function() {
-        debugger;
-        $('.dropdown-menu.show>div').removeClass('show');
-        $(this).next().addClass('show');
+    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+        if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+            $('.dropdown-submenu .show').removeClass("show");
+        });
+        return false;
     });
 });
